64feae5296c3b5f820e9701fad5f8e2aa5e36e0e
Joana Palero <joana.palero24@gmail.com>
2018-07-08 17:00:51 +0000

    automatically backed up by learn

--- lib/doctor.rb
+++ lib/doctor.rb
@@ -17,7 +17,7 @@ class Doctor
   end
   
   def appointments
-    Appointment.all.select {|appt| appt.doctor == self}
+    Appointment.all.select {|appointment| appointment.doctor == self}
   end
   
   def patients
