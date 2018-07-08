f4490e33ceed67303382c5accf198a666578ddfe
Joana Palero <joana.palero24@gmail.com>
2018-07-08 16:43:52 +0000

    automatically backed up by learn

--- lib/patient.rb
+++ lib/patient.rb
@@ -16,5 +16,11 @@ class Patient
     Appointment.new(date, self, doctor)
   end
   
+  def appointments
+    Appointment.all.select{|appt| appt.patient == self}
+  end
   
+  def doctors
+    self.appointments.collect{|appt| appt.doctor}
+  end
 end
